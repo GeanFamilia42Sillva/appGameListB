190b0ee80d7afe0a43a2c1f2fdab90b633247a5c
Família Gean42Silva <gean42sillva@gmail.com>
2026-06-08 21:45:36 -0300

    react_aula0806

---
+++ App.js
@@ -0,0 +1,40 @@
+import { View, Text } from 'react-native' 
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+const HomeScreen = () => {
+  return(
+    <View>
+      <Text>Tela Home</Text>      
+    </View>
+  )
+}
+
+const SobreScreen = () => {
+  return(
+    <View>
+      <Text>Tela Sobre</Text>      
+    </View>
+  )
+}
+const Drawer = createDrawerNavigator()
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator 
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#1d3557'
+          },
+          headerTintColor: '#fff',
+          drawerActiveTintColor: '#e66946'
+        }}
+      >
+        <Drawer.Screen name="Home" component={HomeScreen}/>
+        <Drawer.Screen name="Sobre" component={SobreScreen}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
